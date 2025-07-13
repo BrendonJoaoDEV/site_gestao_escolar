db0b734fb845c70a6efe279819680fc83d90444e
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-07-13 02:47:56 -0300

    FEAT: removerAluno

--- src/modules/alunos.js
+++ src/modules/alunos.js
@@ -2,3 +2,25 @@ export function adicionarAluno (bancoDados, alunoAdicionar) {
     bancoDados.push(alunoAdicionar);
     return bancoDados
 }
+
+export function removerAluno (bancoDados, idAlunoRemover) {
+    // Encontra o aluno que deve ser removido pelo seu id
+    const alunoRemover = bancoDados.find((aluno, indice) => {
+        return aluno.id === idAlunoRemover;
+    });
+
+    // Remove o aluno que deve ser removido
+    bancoDados.splice(alunoRemover.id-1, 1)
+
+    // Lista os alunos depois do aluno removido
+    const alunosDepoisRemovido = bancoDados.filter((aluno) => {
+        return aluno.id > idAlunoRemover;
+    });
+
+    // Redefine os ids dos alunos depois do aluno removido para q eles continuem em uma contagem
+    alunosDepoisRemovido.map((aluno) => {
+        return aluno.id = aluno.id -1;
+    });
+
+    return bancoDados
+}
