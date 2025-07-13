7dafdc04209b3d15a45e5dc6ef660ecaae96cc7a
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-07-13 02:16:11 -0300

    TEST: Teste da integração e funcionalidade das funções anteriores

--- src/main.js
+++ src/main.js
@@ -1,29 +1,14 @@
 import { adicionarAluno } from './modules/alunos.js';
+import { carregarAlunos } from './utils/utils.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const entradaNome = document.getElementById('nomeAluno');
-    const entradaNota1 = document.getElementById('nota1Bim');
-    const entradaNota2 = document.getElementById('nota2Bim');
-    const entradaNota3 = document.getElementById('nota3Bim');
-    const entradaNota4 = document.getElementById('nota4Bim')
-    const entradaFrequencia = document.getElementById('frequenciaAluno')
+    let bd = new Array;
 
-    let bd = []
+    const tabela = document.getElementById('tableBody');
+    
+    const aluno = {id: 1, nome: 'teste', frequencia: 80, notas: [20, 10, 30, 0], media: 15, total: 60, situacao: 'Aprovado'};
 
-    document.getElementById('adicionarAluno').addEventListener('click', () => {
-        let nome = entradaNome.value;
-        let nota1 = entradaNota1.value;
-        let nota2 = entradaNota2.value;
-        let nota3 = entradaNota3.value;
-        let nota4 = entradaNota4.value;
-        let frequencia = entradaFrequencia.value;
+    bd = adicionarAluno(bd, aluno);
 
-        aluno = {id: 1, nome: nome, notas: [nota1, nota2, nota3, nota4], frequencia: frequencia}
-
-        adicionarAluno(bd, aluno)
-    })
-})
-
-// Pegar os dados das entradas
-// Validar os dados das entradas
-// Adicionar os dados ao banco
+    carregarAlunos(bd, tabela);
+});
