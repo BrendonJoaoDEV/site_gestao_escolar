2ac7d29ea33830c07f4717bb0b9930fd58fc6e5d
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-07-13 02:48:32 -0300

    TEST: Teste básico da função removerAluno

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import { adicionarAluno } from './modules/alunos.js';
 import { carregarAlunos } from './utils/utils.js';
+import { adicionarAluno, removerAluno } from './modules/alunos.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     let bd = new Array;
 
     const tabela = document.getElementById('tableBody');
-    
-    const aluno = {id: 1, nome: 'teste', frequencia: 80, notas: [20, 10, 30, 0], media: 15, total: 60, situacao: 'Aprovado'};
 
-    bd = adicionarAluno(bd, aluno);
+    bd = adicionarAluno(bd, {id: 1, nome: 'teste1', frequencia: 80, notas: [20, 10, 30, 0], media: 15, total: 60, situacao: 'Aprovado'});
+    bd = adicionarAluno(bd, {id: 2, nome: 'teste2', frequencia: 80, notas: [20, 10, 30, 0], media: 15, total: 60, situacao: 'Aprovado'});
+    bd = adicionarAluno(bd, {id: 3, nome: 'teste3', frequencia: 80, notas: [20, 10, 30, 0], media: 15, total: 60, situacao: 'Aprovado'});
+    bd = adicionarAluno(bd, {id: 4, nome: 'teste4', frequencia: 80, notas: [20, 10, 30, 0], media: 15, total: 60, situacao: 'Aprovado'});
+    bd = adicionarAluno(bd, {id: 5, nome: 'teste5', frequencia: 80, notas: [20, 10, 30, 0], media: 15, total: 60, situacao: 'Aprovado'});
 
-    carregarAlunos(bd, tabela);
+    bd = removerAluno(bd, 3);
+
+    carregarAlunos(bd, tabela)
 });
