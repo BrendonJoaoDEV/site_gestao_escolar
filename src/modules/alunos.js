726519e35febc57c68b83011392422e110889b0e
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-07-14 20:59:19 -0300

    FEAT: alterarAlunos

--- src/modules/alunos.js
+++ src/modules/alunos.js
@@ -3,6 +3,18 @@ export function adicionarAluno (estruturaDados, alunoAdicionar) {
     return estruturaDados;
 }
 
+export function alterarAluno (estruturaDados, idAlunoAlterar, novosDadosAluno) {
+    // Encontra o aluno que deve ser alterado
+    const alunoAlterar = estruturaDados.find((aluno) => {
+        return aluno.id === idAlunoAlterar;
+    });
+
+    // Remove os dados anteriores do aluno e substitui pelos novos dados
+    estruturaDados.splice(alunoAlterar.id-1, 1, novosDadosAluno);
+
+    return estruturaDados;
+}
+
 export function removerAluno (estruturaDados, idAlunoRemover) {
     // Encontra o aluno que deve ser removido pelo seu id
     const alunoRemover = estruturaDados.find((aluno) => {
