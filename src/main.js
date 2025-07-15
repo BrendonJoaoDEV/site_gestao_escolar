4f73613188f1952d0e5a02be0ac2a56e3e6a480d
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-07-15 20:37:01 -0300

    FEAT: Funcionalidade para adição de alunos no main

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
+import { botaoEvento, carregarAlunos} from "./utils/utils.js";
+import { adicionarAluno, alterarAluno, removerAluno } from "./modules/alunos.js";
 import { calcularMedia } from "./modules/calculo.js";
-import { botaoEvento, printTabela } from "./utils/utils.js";
-import { printResultado } from "./utils/utils.js";
-import { carregarAlunos } from './utils/utils.js';
-import { adicionarAluno, alterarAluno } from './modules/alunos.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const tabela = document.getElementById('tableBody');
+  const entradaNome = document.getElementById('nomeAluno');
+  const entradaFrequencia = document.getElementById('frequenciaAluno');
+  const entradaNota1 = document.getElementById('nota1Bim');
+  const entradaNota2 = document.getElementById('nota2Bim');
+  const entradaNota3 = document.getElementById('nota3Bim');
+  const entradaNota4 = document.getElementById('nota4Bim');
+  let estruturaDados = new Array;
+
   botaoEvento('adicionarAluno', () => {
-    const media = calcularMedia();
-    if (media !== null) {
-      printTabela(media.toFixed(2));
-    }
+    let arrayNotas = [entradaNota1.value, entradaNota2.value, entradaNota3.value, entradaNota4.value];
+    
+    let aluno = {
+      // id: ,
+      nome: entradaNome.value,
+      frequencia: entradaFrequencia.value,
+      notas: arrayNotas,
+      media: calcularMedia(arrayNotas),
+      total: arrayNotas.reduce((soma, n) => soma + n)
+      // situacao:
+    };
+
+    estruturaDados = adicionarAluno(estruturaDados, aluno);
+
+    carregarAlunos(estruturaDados, tabela);
   });
 });
