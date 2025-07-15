c2e3257f42dd2f7cfb16a97945b43440676ddb5d
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-07-15 19:59:33 -0300

    REFACTOR: Refatoração da função calcularMedia para aceitar um array como parâmetro

--- src/modules/calculo.js
+++ src/modules/calculo.js
@@ -1,13 +1,13 @@
 import { printResultado } from "../utils/utils.js";
 
-export function calcularMedia(nota1, nota2, nota3, nota4) {
-  const notas = [nota1, nota2, nota3, nota4].map(n => parseFloat(n));
+export function calcularMedia(notas) {
+  const notasConvertidas = notas.map(n => parseFloat(n));
 
-  if (notas.some(n => isNaN(n) || n < 0 )) {
+  if (notasConvertidas.some(n => isNaN(n) || n < 0 )) {
     printResultado('erro', 'Notas inválidas');
     return null;
   }
 
-  const media = notas.reduce((soma, n) => soma + n, 0) / notas.length;
+  const media = notasConvertidas.reduce((soma, n) => soma + n, 0) / notasConvertidas.length;
   return media;
 }
