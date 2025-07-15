90470741471b82f56eb15f62103511ea8e625b99
daniloamarojf <daniloramarojf@gmail.com>
2025-07-15 20:20:16 -0300

    FAET: Verificar Situação

--- src/modules/situacao.js
+++ src/modules/situacao.js
@@ -1,1 +1,13 @@
+export function verificarSituacao (estruturaDados) {
+    const frequencia = estruturaDados.frequencia;
+    const media = estruturaDados.media;
 
+
+    if (frequencia >=75 && media >= 60) {
+        return 'aprovado';
+    } else {
+        return 'Reprovado';
+
+    } 
+
+}
