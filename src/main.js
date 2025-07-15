feb2722e60829ff206c9ee5fca8cae92a839670b
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-07-14 21:00:05 -0300

    TEST: Teste básico da função alterarAluno

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { carregarAlunos } from './utils/utils.js';
-import { adicionarAluno, removerAluno } from './modules/alunos.js';
+import { adicionarAluno, alterarAluno } from './modules/alunos.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     let bd = new Array;
@@ -12,7 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
     bd = adicionarAluno(bd, {id: 4, nome: 'teste4', frequencia: 80, notas: [20, 10, 30, 0], media: 15, total: 60, situacao: 'Aprovado'});
     bd = adicionarAluno(bd, {id: 5, nome: 'teste5', frequencia: 80, notas: [20, 10, 30, 0], media: 15, total: 60, situacao: 'Aprovado'});
 
-    bd = removerAluno(bd, 3);
+    let novosDadosAluno = {id: 3, nome: 'testeAlterado', frequencia: 80, notas: [20, 10, 30, 0], media: 15, total: 60, situacao: 'Aprovado'};
+
+    bd = alterarAluno(bd, 3, novosDadosAluno);
 
     carregarAlunos(bd, tabela)
 });
