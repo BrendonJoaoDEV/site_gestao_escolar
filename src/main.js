6b2b8a24be3539f4b40591f42e1b171d1732fdff
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-07-16 10:46:35 -0300

    FEAT: Correção na junção do main

--- src/main.js
+++ src/main.js
@@ -1,37 +1,5 @@
-// Variável para controlar o último ID usado
-let ultimoId = 0;
-
-document.addEventListener('DOMContentLoaded', () => {
-  const tabela = document.getElementById('tableBody');
-  const entradaNome = document.getElementById('nomeAluno');
-  const entradaFrequencia = document.getElementById('frequenciaAluno');
-  const entradaNota1 = document.getElementById('nota1Bim');
-  const entradaNota2 = document.getElementById('nota2Bim');
-  const entradaNota3 = document.getElementById('nota3Bim');
-  const entradaNota4 = document.getElementById('nota4Bim');
-  let estruturaDados = new Array;
-
-  acessibilidade();
-  botaoEvento('adicionarAluno', () => {
-    let arrayNotas = [entradaNota1.value, entradaNota2.value, entradaNota3.value, entradaNota4.value];
-    
-    let aluno = {
-      // id: ,
-      nome: entradaNome.value,
-      frequencia: entradaFrequencia.value,
-      notas: arrayNotas,
-      media: calcularMedia(arrayNotas),
-      total: arrayNotas.reduce((soma, n) => soma + n)
-      // situacao:
-    };
-
-    estruturaDados = adicionarAluno(estruturaDados, aluno);
-
-    carregarAlunos(estruturaDados, tabela);
-  });
-});
-
 import { botaoEvento, carregarAlunos} from "./utils/utils.js";
+import { acessibilidade } from "./modules/acessibilidade.js"
 import { gerarIdUnico, adicionarAluno, alterarAluno, removerAluno } from "./modules/alunos.js";
 import { calcularMedia } from "./modules/calculo.js";
 import { filtrarSituacao } from "./modules/filtro.js";
@@ -50,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const entradaNota4 = document.getElementById('nota4Bim');
   let estruturaDados = new Array;
 
+  acessibilidade();
   botaoEvento('adicionarAluno', () => {
     let idUnico = 0;
 
@@ -77,11 +46,8 @@ document.addEventListener('DOMContentLoaded', () => {
       situacao: situacaoAluno
     };
 
-    console.log(tabela)
-
     estruturaDados = adicionarAluno(estruturaDados, aluno);
     
     carregarAlunos(estruturaDados, tabela);
   });
-
 });
