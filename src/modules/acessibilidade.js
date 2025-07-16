81d1bb6af896309c948f689a18eb0473d9ab71dd
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-07-16 11:35:54 -0300

    FEAT: Correção na mudança de aumento da fonte

--- src/modules/acessibilidade.js
+++ src/modules/acessibilidade.js
@@ -11,15 +11,17 @@ export function acessibilidade() {
   const btnAumentar = document.getElementById('aumentar-fonte');
   const btnDiminuir = document.getElementById('diminuir-fonte');
 
-  let tamanhoFonte = 16; // valor inicial (px)
+  let tamanhoFonte = 20; // valor inicial (px)
 
   btnAumentar.addEventListener('click', () => {
     tamanhoFonte += 2;
-    document.body.style.fontSize = tamanhoFonte + 'px';
+    const main = document.querySelector('main');
+    main.style.fontSize = tamanhoFonte + 'px';
   });
 
   btnDiminuir.addEventListener('click', () => {
     tamanhoFonte = Math.max(12, tamanhoFonte - 2); // mínimo 12px para não ficar muito pequeno
-    document.body.style.fontSize = tamanhoFonte + 'px';
+    const main = document.querySelector('main');
+    main.style.fontSize = tamanhoFonte + 'px';
   });
 }
