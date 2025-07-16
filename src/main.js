2c86228213a5043258cd82db3d4343024fbbf66a
brunolourencosilva <brunolourenco.silva15@gmail.com>
2025-07-16 15:52:22 -0300

    FEAT: Botão remover funcionando

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import { botaoEvento, carregarAlunos} from "./utils/utils.js";
-import { acessibilidade } from "./modules/acessibilidade.js"
+import { botaoEvento, carregarAlunos } from "./utils/utils.js";
+import { acessibilidade } from "./modules/acessibilidade.js";
 import { adicionarAluno, alterarAluno, removerAluno } from "./modules/alunos.js";
 import { calcularMedia, gerarIdUnico } from "./modules/calculo.js";
 import { filtrarSituacao } from "./modules/filtro.js";
 import { verificarSituacao } from "./modules/situacao.js";
 
-// Variável para controlar o último ID usado
-let ultimoId = 0;
+let estruturaDados = [];
 
 document.addEventListener('DOMContentLoaded', () => {
   const tabela = document.getElementById('tableBody');
@@ -16,38 +15,38 @@ document.addEventListener('DOMContentLoaded', () => {
   const entradaNota2 = document.getElementById('nota2Bim');
   const entradaNota3 = document.getElementById('nota3Bim');
   const entradaNota4 = document.getElementById('nota4Bim');
-  let estruturaDados = new Array;
 
   acessibilidade();
+
+  function atualizarTabela() {
+    carregarAlunos(estruturaDados, tabela, (idRemover) => {
+      estruturaDados = removerAluno(estruturaDados, idRemover);
+      atualizarTabela(); // recarrega com nova estrutura
+    });
+  }
+
   botaoEvento('adicionarAluno', () => {
     let idUnico = 0;
 
-    if (estruturaDados) {
-      if (estruturaDados.length > 0) {
-        let copiaEstruturaDados = estruturaDados.slice()
-        let ultimoAluno = copiaEstruturaDados.pop();
-        if (ultimoAluno === undefined) {
-          idUnico = 0;
-        } else {
-          idUnico = ultimoAluno.id;
-        }
-      } else {
-        idUnico = 0;
-      }
+    if (estruturaDados.length > 0) {
+      let copiaEstruturaDados = estruturaDados.slice();
+      let ultimoAluno = copiaEstruturaDados.pop();
+      idUnico = ultimoAluno ? ultimoAluno.id : 0;
     }
 
     idUnico = gerarIdUnico(idUnico);
 
     let nomeAluno = entradaNome.value;
-
     let frequenciaAluno = entradaFrequencia.value;
+    let arrayNotas = [
+      Number(entradaNota1.value),
+      Number(entradaNota2.value),
+      Number(entradaNota3.value),
+      Number(entradaNota4.value)
+    ];
 
-    let arrayNotas = [entradaNota1.value, entradaNota2.value, entradaNota3.value, entradaNota4.value];
-    
     let mediaAluno = calcularMedia(arrayNotas);
-
-    let totalAluno = arrayNotas.reduce((soma, n) => soma + n);
-
+    let totalAluno = arrayNotas.reduce((soma, n) => soma + n, 0);
     let situacaoAluno = verificarSituacao(mediaAluno, frequenciaAluno);
 
     let aluno = {
@@ -61,7 +60,6 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     estruturaDados = adicionarAluno(estruturaDados, aluno);
-    
-    carregarAlunos(estruturaDados, tabela);
+    atualizarTabela();
   });
 });
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,24 +12,38 @@ export function limparTabela (tabela) {
   tabela.innerHTML = '';
 }
 
-export function carregarAlunos (bancoDados, tabelaHtml) {
+export function carregarAlunos(bancoDados, tabelaHtml, aoRemover) {
   if (tabelaHtml) {
     limparTabela(tabelaHtml);
     bancoDados.forEach((aluno) => {
-        tabelaHtml.innerHTML += `
-      <tr>
-        <td>${aluno.id}</td>
-        <td>${aluno.nome}</td>
-        <td>${aluno.frequencia}</td>
-        <td>${aluno.notas}</td>
-        <td>${aluno.media}</td>
-        <td>${aluno.total}</td>
-        <td>${aluno.situacao}</td>
-        <td>
-            <button type="button" class="apagarDados">apagar</button>
-            <button type="button" class="alterarDados">alterar</button>
-        </td>
-      </tr>`;
+      tabelaHtml.innerHTML += `
+        <tr>
+          <td>${aluno.id}</td>
+          <td>${aluno.nome}</td>
+          <td>${aluno.frequencia}</td>
+          <td>${aluno.notas}</td>
+          <td>${aluno.media}</td>
+          <td>${aluno.total}</td>
+          <td>${aluno.situacao}</td>
+          <td>
+              <button type="button" class="apagarDados" data-id="${aluno.id}">apagar</button>
+              <button type="button" class="alterarDados" data-id="${aluno.id}">alterar</button>
+          </td>
+        </tr>`;
     });
+
+    // Adiciona eventos de apagar com confirmação
+    if (typeof aoRemover === 'function') {
+      document.querySelectorAll('.apagarDados').forEach(botao => {
+        botao.addEventListener('click', (event) => {
+          const id = Number(event.target.getAttribute('data-id'));
+
+          const confirmar = confirm("Tem certeza que deseja apagar este aluno?");
+          if (confirmar) {
+            aoRemover(id);
+          }
+        });
+      });
+    }
   }
 }
