24822ac485297622b31b67d0d5883a3168acab22
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-07-16 21:18:02 -0300

    FEAT: Correção da validação de nome

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,61 +1,63 @@
-export function validarNota (nota1Bim, nota2Bim, nota3Bim, nota4Bim) {
-    
-    // Campos vazios
-    if (nota1Bim === '' || nota2Bim === '' || nota3Bim === '' || nota4Bim === '') {
-        alert("Todos os campos devem ser preenchidos.")
-        return null;
-    }
+export function validarNota(nota1Bim, nota2Bim, nota3Bim, nota4Bim) {
 
-    // Converter em número
-    [nota1Bim, nota2Bim, nota3Bim, nota4Bim] = 
+  // Campos vazios
+  if (nota1Bim === '' || nota2Bim === '' || nota3Bim === '' || nota4Bim === '') {
+    alert("Todos os campos devem ser preenchidos.")
+    return null;
+  }
+
+  // Converter em número
+  [nota1Bim, nota2Bim, nota3Bim, nota4Bim] =
     [nota1Bim, nota2Bim, nota3Bim, nota4Bim].map(Number);
 
-    // Números válidos
-    if (isNaN(nota1Bim) || isNaN(nota2Bim) || isNaN(nota3Bim) || isNaN(nota4Bim)) {
-        alert("Por favor, preencha o campo corretamente.")
-        return null;
-    }
+  // Números válidos
+  if (isNaN(nota1Bim) || isNaN(nota2Bim) || isNaN(nota3Bim) || isNaN(nota4Bim)) {
+    alert("Por favor, preencha o campo corretamente.")
+    return null;
+  }
 
-    // Número negativo
-    if (nota1Bim < 0 || nota2Bim < 0 || nota3Bim < 0 || nota4Bim < 0) {
-        alert("Nota do aluno não pode ser negativa.")
-        return null;
-    }
+  // Número negativo
+  if (nota1Bim < 0 || nota2Bim < 0 || nota3Bim < 0 || nota4Bim < 0) {
+    alert("Nota do aluno não pode ser negativa.")
+    return null;
+  }
 
-    // Número maior que 25
-    if (nota1Bim > 25 || nota2Bim > 25 || nota3Bim > 25 || nota4Bim > 25) {
-        alert("Nota do aluno não pode ser maior que 25 pontos.")
-        return null;
+  // Número maior que 25
+  if (nota1Bim > 25 || nota2Bim > 25 || nota3Bim > 25 || nota4Bim > 25) {
+    alert("Nota do aluno não pode ser maior que 25 pontos.")
+    return null;
 
-    }
+  }
 
-    return {nota1Bim, nota2Bim, nota3Bim, nota4Bim};
+  return { nota1Bim, nota2Bim, nota3Bim, nota4Bim };
 }
-    
+
 
 export function validarNome(nome) {
 
-    // Letras com acentos, espaços e hífem
-    const regex = /^[a-zÀ-ÿ\s'-]+$/;
+  // Letras com acentos, espaços e hífem
+  // const regex = /^[a-zÀ-ÿ\s'-]+$/;
+  const regex = /^[\p{L} ]+$/u;
 
-    if (!regex.test(nome)) {
-        alert('O nome não deve conter números ou caracteres especiais.');
-        return null;
-    }
+  if (!regex.test(nome)) {
+    alert('O nome não deve conter números ou caracteres especiais.');
+    return null;
+  }
 
-    // Dividindo em partes, sem espaço no icicio e final
-    const nomePartes = nome.trim().split(/\s+/);
+  // Dividindo em partes, sem espaço no icicio e final
+  const nomePartes = nome.trim().split(/\s+/);
 
-    // Se há no mínino duas palavras
-    // Cada com duas letras
-    const nomeValido = nomePartes.length >=2 && nomePartes.every(parte => parte.length >= 2);
-    
-    // Mensagem se nome inválido
-    if (!nomeValido) {
-        alert('Digite o nome completo (nome e sobrenome).')
-        return null;
-    }
-    return nome;
+  // Se há no mínino duas palavras
+  // Cada com duas letras
+  const nomeValido = nomePartes.length >= 2 && nomePartes.every(parte => parte.length >= 2);
+
+  // Mensagem se nome inválido
+  if (!nomeValido) {
+    alert('Digite o nome completo (nome e sobrenome).')
+    return null;
+  }
+  return nome;
+}
 
 export function botaoEvento(idbotao, funcao) {
   const botao = document.getElementById(idbotao);
@@ -67,7 +69,7 @@ export function printResultado(id, print) {
   if (elemento) elemento.innerHTML = print;
 }
 
-export function limparTabela (tabela) {
+export function limparTabela(tabela) {
   tabela.innerHTML = '';
 }
 
